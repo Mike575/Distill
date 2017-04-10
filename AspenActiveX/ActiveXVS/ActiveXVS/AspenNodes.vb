d4b32c2e1c52af11a924eb5e65947cb974e08a9e
Mike575 <575840864@qq.com>
2017-04-10 16:15:25 +0800

    add AspenNodes

---
+++ AspenActiveX/ActiveXVS/ActiveXVS/AspenNodes.vb
@@ -0,0 +1,56 @@
+# node name
+Application.Tree.Data.Blocks.B1.Input.LOWER     '80     
+Application.Tree.Data.Blocks.B1.Input.NSTAGE    '200
+Application.Tree.Data.Blocks.B1.Input.HEAVYKEY  'BENYIXI
+Application.Tree.Data.Blocks.B1.Input.LIGHTKEY  'YIBEN
+Application.Tree.Data.Blocks.B1.Input.RECOVH    '0.001
+Application.Tree.Data.Blocks.B1.Input.RECOVL    '0.998
+Application.Tree.Data.Blocks.B1.Input.RR        '-1.5
+Application.Tree.Data.Blocks.B1.Input.SIM_LEVEL '4
+Application.Tree.Data.Blocks.B1.Input.UPPER     '200
+
+Application.Tree.Data.Blocks.B1.Input.Unit Set  'METCBAR    
+'Metric units with temperature in degrees Centigrade and pressure in bars       
+
+Application.Tree.Data.Blocks.B1.Input.DUTY_TOL  '#?#    23.88
+Application.Tree.Data.Blocks.B1.Input.INPUT     'INPUT 
+Application.Tree.Data.Blocks.B1.Input.MAXIT     '50
+Application.Tree.Data.Blocks.B1.Input.OPSETNAME 'NRTL
+Application.Tree.Data.Blocks.B1.Input.OPT_NTRR  'RR
+Application.Tree.Data.Blocks.B1.Input.OPT_RDV   'LIQUID
+Application.Tree.Data.Blocks.B1.Input.PBOT      '0.2
+Application.Tree.Data.Blocks.B1.Input.PROP_LEVEL    '4
+Application.Tree.Data.Blocks.B1.Input.PTOP      '0.18
+
+#output 
+Application.Tree.Data.Blocks.B1.Output.RR_OUT       '80~200 stages
+
+Application.Tree.Data.Blocks.B1.Output.BOTTOM_TEMP  'Bottom temperature:92.94
+Application.Tree.Data.Blocks.B1.Output.COND_DUTY    'Condenser cooling required:131783.434
+Application.Tree.Data.Blocks.B1.Output.DISTIL_TEMP  'Distillate temperature:81.8517546
+Application.Tree.Data.Blocks.B1.Output.DIST_VS_FEED 'Distillate to feed fraction:0.29610176
+Application.Tree.Data.Blocks.B1.Output.FEED_LOCATN  'Feed stage:47.4289849
+Application.Tree.Data.Blocks.B1.Output.MIN_REFLUX   'Minimum reflux ratio:11.3628666
+Application.Tree.Data.Blocks.B1.Output.MIN_STAGES   'Minimum number of stages:53.3535077
+Application.Tree.Data.Blocks.B1.Output.REB_DUTY     'Reboiler heating required:139444.64
+Application.Tree.Data.Blocks.B1.Output.RECT_STAGES  'Number of actual stages above feed:46.4289849
+Application.Tree.Data.Blocks.B1.Output.ACT_REFLUX   'Actual reflux ratio:17.0443
+Application.Tree.Data.Blocks.B1.Output.ACT_STAGES   'Number of actual stages:80.9179894
+
+Application.Tree.Data.Blocks.B1.Stream Results.Table
+Application.Tree.Data.Blocks.B1.Stream Results.Table.  BENYIXI FEED     '6.720977
+Application.Tree.Data.Blocks.B1.Stream Results.Table.  BENYIXI BOTTOM   '6.714256
+Application.Tree.Data.Blocks.B1.Stream Results.Table.  YIBEN FEED       '2.825726
+Application.Tree.Data.Blocks.B1.Stream Results.Table.  YIBEN BOTTOM     '0.00565145
+Application.Tree.Data.Blocks.B1.Stream Results.Table.  YIBEN UP         '2.820075
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow kmol/hr FEED    '9.546703
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow kmol/hr BOTTOM  '6.719908
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow kmol/hr UP      '2.826796
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow kg/hr FEED      '1000
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow kg/hr BOTTOM    '699.9
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow kg/hr UP        '300.1
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow l/min FEED      '18.85769
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow l/min BOTTOM    '13.92351
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Total Flow l/min UP        '6.147346
+Application.Tree.Data.Blocks.B1.Stream Results.Table.Temperature C FEED         '30
+
